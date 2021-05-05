08f8f62be93191081f30f25eebc17a268b48dc4c
Neylane <neylanepl@gmail.com>
2021-05-05 19:41:49 -0300

    Static charts

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-<<<<<<< HEAD
 import 'assets/css/styles.css';
-=======
->>>>>>> 78604e53b7798a91453e7173a0e71d6670576eb0
 import App from './App';
 
 ReactDOM.render(
